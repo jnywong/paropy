49b8889b6ef725bcacb1d4e0ce7e25d54a2e7f23
jnywong <jw53119@gmail.com>
2021-01-14 18:02:25 +0100

    update

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from setuptools.command.build_ext import build_ext as _build_ext
-
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
